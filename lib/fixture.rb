68d96ed88bc6f446aeb82bb4db69a6fe98074c73
addicted <addicted22sounds@gmail.com>
2015-05-31 14:48:00 +0300

    alpha ver

--- lib/fixture.rb
+++ lib/fixture.rb
@@ -3,60 +3,66 @@ require 'active_record'
 require 'active_support/inflector'
 Dir['./lib/models/*'].each {|file| require file }
 
-class FixtureFactory
-  class << self
-    attr_reader :fixture_format
-    METHODS_MAP = {
-        ini: :load_ini,
-        json: :load_json
-    }
-    def load(factory)
-
-    end
-
-    def load_single(factory, hash, save=false)
-      model_name = factory.to_s.singularize.camelize
-      model = model_name.constantize.new hash
-      model.save if save
-      model
-    end
-
-    def fixtures_filename(factory)
-      raise NotImplementedError, :fixtures_format_not_set if @fixture_format.nil?
-      "example-#{factory}.#{fixture_format}"
-    end
-
-    def fixtures_format=(type)
-      raise NotImplementedError, :format_not_supported unless METHODS_MAP.keys.include? type
-      @fixture_format = type
-    end
-    # load textures file
-    def load_file(factory, filename)
-      @data ||= Hash.new
-      @data[factory] = Hash.new
-      extension = File.extname(filename)[1..-1].to_sym
-      @fixture_format = extension
-      if METHODS_MAP.include? extension
-        self.send METHODS_MAP[extension], factory, filename
-      else
-        raise InvalidFileTypeError
-      end
-    end
-
-    # Load .json file
-    def load_json(factory, filename)
-      @data[factory] = JSON.parse File.read(filename)
-    end
-
-    # Load .ini file
-    def load_ini(factory, filename)
-      File.open(filename).each { |row|
-        if row.match /^data\[(?<alias>.*?)\]\[(?<name>.*?)\]\s+=\s+(?<value>.*?)\s+$/
-          @data[factory][:alias] ||= Hash.new
-          @data[factory][:alias][:name] = :value
-        end
+module Fixture
+  class Factory
+    class << self
+      attr_reader :fixture_format
+      METHODS_MAP = {
+          ini: :load_ini,
+          json: :load_json
       }
-      @data.values
+      def load(factory)
+        load_file(factory, fixtures_filename(factory)).each do |attributes|
+          #p attributes
+          load_single factory, attributes, true
+        end
+      end
+
+      def load_single(factory, hash, save=false)
+        model_name = factory.to_s.singularize.camelize
+        model = model_name.constantize.new hash
+        model.save if save
+        model
+      end
+
+      def fixtures_filename(factory)
+        raise NotImplementedError, :fixtures_format_not_set if @fixture_format.nil?
+        "example-#{factory}.#{fixture_format}"
+      end
+
+      def fixtures_format=(type)
+        raise NotImplementedError, :format_not_supported unless METHODS_MAP.keys.include? type
+        @fixture_format = type
+      end
+
+      # load textures file
+      def load_file(factory, filename)
+        @data ||= Hash.new
+        @data[factory] = Hash.new
+        extension = File.extname(filename)[1..-1].to_sym
+        @fixture_format = extension
+        if METHODS_MAP.include? extension
+          self.send METHODS_MAP[extension], factory, filename
+        else
+          raise InvalidFileTypeError
+        end
+      end
+
+      # Load .json file
+      def load_json(factory, filename)
+        @data[factory] = JSON.parse(File.read(filename), symbolize_names: true)
+      end
+
+      # Load .ini file
+      def load_ini(factory, filename)
+        File.open(filename).each { |row|
+          if row.match /^data\[(?<alias>.*?)\]\[(?<name>.*?)\]\s+=\s+(?<value>.*?)\s+$/
+            @data[factory][:alias] ||= Hash.new
+            @data[factory][:alias][:name] = :value
+          end
+        }
+        @data.values
+      end
     end
   end
 end
--- spec/lib/fixture_spec.rb
+++ spec/lib/fixture_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 require 'fixture'
 Dir['./models/*.rb'].each {|file| require file }
 
-describe FixtureFactory do
+describe Fixture::Factory do
   describe '#load_file' do
-    subject { FixtureFactory.load_file :users, filename }
+    subject { Fixture::Factory.load_file :users, filename }
     it 'should raise InvalidFileTypeError for unknown format' do
-      expect { FixtureFactory.load_file :users, 'example.xls' }
+      expect { Fixture::Factory.load_file :users, 'example.xls' }
           .to raise_error InvalidFileTypeError
     end
     # it 'should validate correct format'
@@ -23,25 +23,42 @@ describe FixtureFactory do
   end
   describe '#fixtures_filename' do
     it 'return factory filename correctly' do
-      FixtureFactory.fixtures_format = :json
-      expect(FixtureFactory.fixtures_filename(:users))
+      Fixture::Factory.fixtures_format = :json
+      expect(Fixture::Factory.fixtures_filename(:users))
           .to eq 'example-users.json'
     end
   end
 
-  describe '#load' do
-
-  end
-
   describe '#load_single' do
     let(:attributes) { { name: 'xxx' } }
-    subject(:fixture) { FixtureFactory.send :load_single, :users, attributes }
+    subject(:fixture) { Fixture::Factory.send :load_single, :users, attributes }
 
     it { is_expected.to be_a_kind_of User }
     it { expect(fixture.name).to eq 'xxx' }
     it 'can save record' do
-      user = FixtureFactory.send :load_single, :users, attributes, true
+      user = Fixture::Factory.send :load_single, :users, attributes, true
       expect(user.class.find user.id).to be_a_kind_of user.class
+    end
+  end
+
+  describe '#load' do
+    let(:factory) { :users }
+
+    context 'json format' do
+      before do
+        Fixture::Factory.fixtures_format = :json
+      end
+      it 'load fixtures' do
+        Fixture::Factory.load :users
+      end
+    end
+    context 'ini format' do
+      before do
+        Fixture::Factory.fixtures_format = :json
+      end
+      it 'load fixtures' do
+        Fixture::Factory.load :users
+      end
     end
   end
 end
